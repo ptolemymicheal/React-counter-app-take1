1544f916b60494782aa98690c668e0870cea7c26
Michael <82574480+ptolemymicheal@users.noreply.github.com>
2022-11-11 07:41:49 +0100

    Take 2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 
 import './App.css';
 import React, { useState } from 'react';
+import ErrorBoundary from './ErrorBoundary';
+import PageNotFound from './404';
+
 
 function App() {
   const [count, setCount] = useState(0);
-  const reset= 0
-  
-  
+  const reset = 0
+
+
 
   return (
- 
-    <div className="App">
-      <div className='display'>Count: {count}</div>
-      
-  
-      
-   
-      
-    
-       
-      
+    <ErrorBoundary>
+      <div className="App">
+        <div className='display'>Count: {count}</div>
+        <div>
+          <input type='number' className='inputValue' placeholder='input a number here' value={count} onChange={(e) => setCount(e.target.value)} /> 
+        </div>
+        <div><button className='buttons' id='plus' onClick={() => setCount(count + 1)}>Increment</button></div>
+        <div><button className='buttons' id='minus' onClick={() => setCount(count - 1)}>Decrement</button></div>
+        <div><button className='buttons' id='reset' onClick={() => setCount(reset)}>Reset</button></div>
+        <div><PageNotFound/></div>
+      </div>
+    </ErrorBoundary>
 
-      <div><button className='buttons' id='plus' onClick={() => setCount(count + 1)}>Increment</button></div>
-      <div><button className='buttons' id='minus' onClick={() => setCount(count - 1)}>Decrement</button></div>
-      <div><button className='buttons' id='reset' onClick={() => setCount(reset)}>Reset</button></div>
-      
-    </div>
+   
   );
 }
 
---
+++ src/ErrorBoundary.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) { return { hasError: true }; }
+    componentDidCatch(error, errorInfo) { logErrorToMyService(error, errorInfo); }
+    render() {
+        if (this.state.hasError) { return <h1><span className="Oops">Oops</span>Something went wrong.</h1>; }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
